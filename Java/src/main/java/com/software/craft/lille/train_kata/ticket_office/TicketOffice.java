ce4125f4ef06ec1be829880942e2221779d3d85c
Christophe <christophe.fargeix@gmail.com>
2026-03-19 13:58:10 +0100

    feat: Session kata du midi du jeudi 16 mars 2026: - Ajout des dépendances à TicketOffice pour effectuer une réservation sur les services externes - Négocier une référence de réservation - Appeler la réservation du train avec cette référence - Utiliser la requête de réservation pour effectuer la réservation

--- Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
+++ Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
@@ -1,13 +1,34 @@
 package com.software.craft.lille.train_kata.ticket_office;
 
+import com.software.craft.lille.train_kata.configuration.api.booking_reference.BookingReferenceClient;
+import com.software.craft.lille.train_kata.configuration.api.train_data_service.TrainDataServiceClient;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
 
 @Service
 public class TicketOffice {
-    
+    private final BookingReferenceClient bookingReferenceClient;
+    private final TrainDataServiceClient trainDataServiceClient;
+    private final JsonMapper jsonMapper;
+
+    public TicketOffice(TrainDataServiceClient trainDataServiceClient, BookingReferenceClient bookingReferenceClient, JsonMapper jsonMapper) {
+        this.trainDataServiceClient = trainDataServiceClient;
+        this.bookingReferenceClient = bookingReferenceClient;
+        this.jsonMapper = jsonMapper;
+    }
+
     public Reservation makeReservation(ReservationRequest request) {
-		//TODO: implement this code!
-		return null;
+        String bookingReference = bookingReferenceClient.getBookingReference();
+        String seatsToReserve = jsonMapper.writeValueAsString(List.of("1A", "2A", "3A", "4A"));
+        trainDataServiceClient.reserveSeats(request.trainId, seatsToReserve, bookingReference);
+        return new Reservation(request.trainId,
+                List.of(new Seat("A", 1),
+                        new Seat("A", 2),
+                        new Seat("A", 3),
+                        new Seat("A", 4)),
+                bookingReference);
     }
 
 }
--- Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
+++ Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
@@ -1,12 +1,86 @@
 package com.software.craft.lille.train_kata.ticket_office;
 
+import com.software.craft.lille.train_kata.configuration.api.booking_reference.BookingReferenceClient;
+import com.software.craft.lille.train_kata.configuration.api.train_data_service.TrainDataServiceClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import tools.jackson.databind.json.JsonMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TicketOfficeTest {
+    Reservation reservation = null;
+
+    @Nested
+    class MakeReservationWithNullRequest {
+        @BeforeEach
+        void setUp() {
+            TrainDataServiceClient trainDataServiceClient = new TrainDataServiceClient() {
+                @Override
+                public ResponseEntity<String> dataForTrain(String trainId) {
+                    return null;
+                }
+
+                @Override
+                public void reserveSeats(String trainId, String seatsJson, String bookingReference) {
+
+                }
+
+                @Override
+                public void resetTrain(String trainId) {
+
+                }
+            };
+            BookingReferenceClient bookingReferenceClient = () -> "12345";
+            reservation = new TicketOffice(trainDataServiceClient, bookingReferenceClient, new JsonMapper())
+                    .makeReservation(null);
+        }
+
+        @Test
+        public void reservationShouldNotBeNull() {
+            assertThat(reservation).isNotNull();
+        }
+
+        @Test
+        void reservationShouldHaveANotBlankBookId() {
+            assertThat(reservation.bookingId()).isNotBlank();
+        }
+
+        @Test
+        void reservationShouldHave4Seats() {
+            assertThat(reservation.seats()).hasSize(4);
+        }
+    }
 
     @Test
-    public void reserveSeats() {
-        // TODO: Write this code!
+    void xxx() {
+        TrainDataServiceClient trainDataServiceClient = new TrainDataServiceClient() {
+            @Override
+            public ResponseEntity<String> dataForTrain(String trainId) {
+                return null;
+            }
 
+            @Override
+            public void reserveSeats(String trainId, String seatsJson, String bookingReference) {
+
+            }
+
+            @Override
+            public void resetTrain(String trainId) {
+
+            }
+        };
+        BookingReferenceClient bookingReferenceClient = () -> "12345";
+        TicketOffice ticketOffice = new TicketOffice(trainDataServiceClient, bookingReferenceClient, new JsonMapper());
+
+        ReservationRequest reservationRequest = new ReservationRequest("local_1000", 4);
+
+        Reservation reservation = ticketOffice.makeReservation(reservationRequest);
+
+        assertThat(reservation.bookingId()).isEqualTo("12345");
+        assertThat(reservation.seats()).hasSize(4);
+        assertThat(reservation.trainId()).isEqualTo("local_1000");
     }
 }
