be3cd49ef20d75735307d9aba9d41e1ff8d3ebf6
Christophe <christophe.fargeix@gmail.com>
2026-05-01 12:05:17 +0200

    feat: Implémentation du premier scénario → création de la réservation à partir de la réponse à la requête du service de réservation

--- Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
+++ Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
@@ -1,24 +1,48 @@
 package com.software.craft.lille.train_kata.ticket_office;
 
 import com.software.craft.lille.train_kata.api.BookingReferenceClient;
+import com.software.craft.lille.train_kata.api.TrainDataServiceClient;
+import com.software.craft.lille.train_kata.api.model.DataForTrain;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.json.JsonMapper;
 
-import java.util.stream.IntStream;
+import java.util.Optional;
+
+import static java.util.function.Predicate.not;
 
 @Component
 public class TicketOffice {
     private final BookingReferenceClient bookingReferenceClient;
+    private final TrainDataServiceClient trainService;
 
-    public TicketOffice(BookingReferenceClient bookingReferenceClient) {
+    public TicketOffice(BookingReferenceClient bookingReferenceClient, TrainDataServiceClient trainDataServiceClient) {
         this.bookingReferenceClient = bookingReferenceClient;
-
+        this.trainService = trainDataServiceClient;
     }
 
     public Reservation makeReservation(ReservationRequest reservationRequest) {
+        String bookingReference = bookingReferenceClient.getBookingReference();
+        ResponseEntity<String> response = trainService.reserveSeats(reservationRequest.trainId(), "{}", bookingReference);
+        DataForTrain dataForTrain = new JsonMapper().readValue(response.getBody(), DataForTrain.class);
         return new Reservation(reservationRequest.trainId(),
-                IntStream.rangeClosed(1, reservationRequest.seatCount())
-                        .mapToObj(seatNumber -> new Seat("a coach", seatNumber))
+                dataForTrain.seats()
+                        .values()
+                        .stream()
+                        .filter(this::hasBeenBooked)
+                        .map(this::toSeat)
                         .toList(),
-                bookingReferenceClient.getBookingReference());
+                bookingReference);
+    }
+
+    private Seat toSeat(DataForTrain.Seat value) {
+        return new Seat(value.coach(), value.seatNumber());
+    }
+
+    private boolean hasBeenBooked(DataForTrain.Seat value) {
+        return Optional.ofNullable(value)
+                .map(DataForTrain.Seat::bookingReference)
+                .filter(not(String::isBlank))
+                .isPresent();
     }
 }
--- Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
+++ Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
@@ -1,6 +1,8 @@
 package com.software.craft.lille.train_kata.ticket_office;
 
+import com.software.craft.lille.train_kata.api.TrainDataServiceClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,7 +10,29 @@ class TicketOfficeTest {
 
     @Test
     public void makeReservation_affectBookingReferenceToTheReservation_fromBookingReferenceService() {
-        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference");
+        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference",
+                new TrainDataServiceClient() {
+                    @Override
+                    public ResponseEntity<String> dataForTrain(String trainId) {
+                        return null;
+                    }
+
+                    @Override
+                    public ResponseEntity<String> reserveSeats(String trainId, String seatsJson, String bookingReference) {
+                        return ResponseEntity.ok(
+                                """
+                                        {
+                                          "seats": {}
+                                        }
+                                        """
+                        );
+                    }
+
+                    @Override
+                    public void resetTrain(String trainId) {
+
+                    }
+                });
 
         Reservation reservation = ticketOffice.makeReservation(new ReservationRequest("1", 1));
 
@@ -18,7 +42,93 @@ class TicketOfficeTest {
 
     @Test
     public void makeReservation_bookTheRequestedNumberOfSeatsOnATrain_onTheSameCoach() {
-        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference");
+        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference",
+                new TrainDataServiceClient() {
+                    @Override
+                    public ResponseEntity<String> dataForTrain(String trainId) {
+                        return null;
+                    }
+
+                    @Override
+                    public ResponseEntity<String> reserveSeats(String trainId, String seatsJson, String bookingReference) {
+                        return ResponseEntity.ok(
+                                """
+                                        {
+                                          "seats": {
+                                            "1a coach": {
+                                              "booking_reference": "a booking reference",
+                                              "seat_number": "1",
+                                              "coach": "a coach"
+                                            },
+                                            "2a coach": {
+                                              "booking_reference": "a booking reference",
+                                              "seat_number": "2",
+                                              "coach": "a coach"
+                                            }
+                                          }
+                                        }
+                                        """
+                        );
+                    }
+
+                    @Override
+                    public void resetTrain(String trainId) {
+
+                    }
+                });
+
+        Reservation reservation = ticketOffice.makeReservation(new ReservationRequest("requestedTrain", 2));
+
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.trainId()).isEqualTo("requestedTrain");
+        assertThat(reservation.seats()).isNotNull().hasSize(2)
+                .containsExactlyInAnyOrder(
+                        new Seat("a coach", 1),
+                        new Seat("a coach", 2)
+                );
+    }
+
+    @Test
+    public void makeReservation_bookTheRequestedNumberOfSeatsOnATrainOnTheSameCoach_usingTrainService() {
+        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference",
+                new TrainDataServiceClient() {
+                    @Override
+                    public ResponseEntity<String> dataForTrain(String trainId) {
+                        return null;
+                    }
+
+                    @Override
+                    public ResponseEntity<String> reserveSeats(String trainId, String seatsJson, String bookingReference) {
+                        return ResponseEntity.ok(
+                                """
+                                        {
+                                          "seats": {
+                                            "1a coach": {
+                                              "booking_reference": "a booking reference",
+                                              "seat_number": "1",
+                                              "coach": "a coach"
+                                            },
+                                            "2a coach": {
+                                              "booking_reference": "a booking reference",
+                                              "seat_number": "2",
+                                              "coach": "a coach"
+                                            },
+                                            "3a coach": {
+                                              "booking_reference": "",
+                                              "seat_number": "3",
+                                              "coach": "a coach"
+                                            }
+                                          }
+                                        }
+                                        """
+                        );
+                    }
+
+                    @Override
+                    public void resetTrain(String trainId) {
+
+                    }
+                });
 
         Reservation reservation = ticketOffice.makeReservation(new ReservationRequest("requestedTrain", 2));
 
