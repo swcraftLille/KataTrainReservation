8166c337bff5bfe8b3b87b8744c9bd7220df234c
Christophe <christophe.fargeix@gmail.com>
2026-05-01 11:29:27 +0200

    feat: Implémentation du premier scénario → négociation de la référence de réservation depuis le service dédié

--- Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
+++ Java/src/main/java/com/software/craft/lille/train_kata/ticket_office/TicketOffice.java
@@ -1,13 +1,21 @@
 package com.software.craft.lille.train_kata.ticket_office;
 
+import com.software.craft.lille.train_kata.api.BookingReferenceClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class TicketOffice {
+    private final BookingReferenceClient bookingReferenceClient;
+
+    public TicketOffice(BookingReferenceClient bookingReferenceClient) {
+        this.bookingReferenceClient = bookingReferenceClient;
+
+    }
+
     public Reservation makeReservation(ReservationRequest reservationRequest) {
         // Implements me
-        return new Reservation("", List.of(), "booking reference");
+        return new Reservation("", List.of(), bookingReferenceClient.getBookingReference());
     }
 }
--- Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
+++ Java/src/test/java/com/software/craft/lille/train_kata/ticket_office/TicketOfficeTest.java
@@ -8,11 +8,11 @@ class TicketOfficeTest {
 
     @Test
     public void makeReservation_affectBookingReferenceToTheReservation_fromBookingReferenceService() {
-        TicketOffice ticketOffice = new TicketOffice();
+        TicketOffice ticketOffice = new TicketOffice(() -> "new booking reference");
 
         Reservation reservation = ticketOffice.makeReservation(new ReservationRequest("1", 1));
 
         assertThat(reservation).isNotNull();
-        assertThat(reservation.bookingId()).isNotBlank();
+        assertThat(reservation.bookingId()).isEqualTo("new booking reference");
     }
 }
